1ad213cf5fbb8ee63bffbff6d9ecc4fdf4c2c350
nnnnnzo <lagadecenzo@gmail.com>
2023-01-27 16:13:39 +0100

    Interface

---
+++ ClientConvertisseurV1/Models/Devise.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string? nomDevise;
+
+		[Required]
+		public string? NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+		public Devise()
+		{
+
+		}
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+    }
+}
