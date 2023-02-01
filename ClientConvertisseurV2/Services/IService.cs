f93e5b5f614232708aa59e75fed29b5f825c510b
nnnnnzo <lagadecenzo@gmail.com>
2023-02-01 17:33:13 +0100

    V2 complete

---
+++ ClientConvertisseurV2/Services/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSConvertisseur.Models;
+
+namespace ClientConvertisseurV2.Services
+{
+    public interface IService
+    {
+        Task<List<Devise>> GetDevisesAsync(string nomControleur);
+    }
+}
---
+++ ClientConvertisseurV2/Services/WSService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using WSConvertisseur.Models;
+
+namespace ClientConvertisseurV2.Services
+{
+    public class WSService : IService
+    {
+        private System.Net.Http.HttpClient client;
+        public WSService(string url) {
+            client = new System.Net.Http.HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Devise>> GetDevisesAsync(string nomControleur)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<List<Devise>>(nomControleur);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
